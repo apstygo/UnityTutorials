73f8b68e300a4de783df6a6768bd28521ed47b74
Artem Pstygo <apstygo.dev@gmail.com>
2023-12-25 23:53:24 +0400

    simplify render dispatching

--- Assets/Graph/GPUGraph.cs
+++ Assets/Graph/GPUGraph.cs
@@ -1,5 +1,4 @@
 using System;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GPUGraph: MonoBehaviour {
@@ -23,9 +22,6 @@ public class GPUGraph: MonoBehaviour {
 
     private const int maxResolution = 1000;
 
-    private FunctionLibrary.FunctionName? previousFunction = null;
-    private FunctionLibrary.FunctionName currentFunction = FunctionLibrary.FunctionName.Wave;
-    private float elapsedTime;
     private ComputeBuffer positionBuffer;
     private static readonly int positionsId = Shader.PropertyToID("_Positions");
     private static readonly int resolutionId = Shader.PropertyToID("_Resolution");
@@ -34,10 +30,6 @@ public class GPUGraph: MonoBehaviour {
 
     private Vector3 scale {
         get { return Vector3.one * 2f / resolution; }
-    }
-
-    private bool isTransitioning {
-        get { return previousFunction.HasValue; }
     }
 
     void OnEnable() {
@@ -50,8 +42,6 @@ public class GPUGraph: MonoBehaviour {
     }
 
     void Update() {
-        UpdateFunction();
-
         UpdateFunctionOnGPU();
     }
 
@@ -69,37 +59,16 @@ public class GPUGraph: MonoBehaviour {
         material.SetBuffer(positionsId, positionBuffer);
         material.SetFloat(stepId, step);
 
+        // 2f / resolution accounts for half the size of a cube
+        // sticking out of bounds
         var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
 
-        Graphics.DrawMeshInstancedProcedural(
-            mesh, 
-            0, 
-            material, 
-            bounds,
-            resolution * resolution
-        );
-    }
+        RenderParams rparams = new(material) {
+            worldBounds = bounds,
+            shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
+            receiveShadows = true
+        };
 
-    private void UpdateFunction() {
-        elapsedTime += Time.deltaTime;
-
-        if (!isTransitioning && elapsedTime >= functionDuration) {
-            elapsedTime -= functionDuration;
-            previousFunction = currentFunction;
-            currentFunction = FunctionLibrary.NextFunctionName(currentFunction);
-        } else if (isTransitioning && elapsedTime >= transitionDuration) {
-            elapsedTime -= transitionDuration;
-            previousFunction = null;
-        }
-
-        float time = Time.time;
-
-        FunctionLibrary.Function f;
-
-        if (previousFunction is FunctionLibrary.FunctionName prevFunc) {
-            f = FunctionLibrary.Morph(prevFunc, currentFunction, elapsedTime / transitionDuration);
-        } else {
-            f = FunctionLibrary.GetFunction(currentFunction);
-        }
+        Graphics.RenderMeshPrimitives(rparams, mesh, 0, resolution * resolution);
     }
 }
